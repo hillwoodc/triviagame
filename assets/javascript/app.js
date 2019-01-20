eac9b0c72930adfed4c1a65eb77fc31f7765c52d
Hillwood Chan <hillwoodc@hillwoods-mbp.home>
2019-01-19 23:41:27 -0800

    final commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,136 @@
+var panel = $("#play-area");
+
+$(document).on("click", "#start", function(event){
+  game.start();
+});
+
+$(document).on("click", "#done", function(event){
+  game.done();
+});
+
+var questions = [{
+	question: "Which one is the Marvel first movie?",
+	choices: ["Blackpanther", "Spiderman", "X-Men", "Ironman"],
+	correctAnswer: "Ironman"
+
+}, {	
+
+	question: "Who is Ironman?",
+	choices: ["Peter Parker", "Dr. Strange", "Tony Stark", "Howard Stark"],
+	correctAnswer: "Tony Stark"
+
+}, {
+
+	question: "Which one is the member of the Guardians of the Galaxy?",
+	choices: ["Ironman", "Spiderman", "Captian Marvel", "Groot"],
+	correctAnswer: "Groot"
+
+}, {
+
+	question: "Who kills Loki?",
+	choices: ["Thor", "Thanos", "Captian America", "Odin"],
+	correctAnswer: "Thanos"
+
+}, {
+
+	question: "Who is Spiderman?",
+	choices: ["Tony Stark", "Nick Fury", "Steve Rogers", "Peter Parker"],
+	correctAnswer: "Peter Parker"
+
+}];
+
+var game = {
+	correct: 0,
+	incorrect: 0,
+	counter: 60,
+
+  countdown: function() {
+  	game.counter--;
+  	$("#counter-number").html(game.counter);
+
+  	if (game.counter === 0) {
+  		alert("TIME'S UP");
+  		game.done();
+
+  	}
+  },
+
+  start: function() {
+  	timer = setInterval(game.countdown, 1000);
+  	$('#subcontainer').prepend('<h2>Time Remaining: <span id="counter-number">60</span> Seconds</h2>');
+  	$("#start").remove();
+
+  	for (var i = 0; i < questions.length; i++) {
+      panel.append('<h2>' + questions[i].question + '</h2>');
+      for (var j = 0; j < questions[i].choices.length; j++){
+        panel.append('<input type="radio" name ="question' + '-' + i + '"value="' + questions[i].choices[j] + '">' + questions[i].choices[j]);
+        }
+  		}
+  		panel.append("<button id='done'>DONE</button>");
+      
+  	},
+
+  	done: function() {
+
+  		$.each($("input[name='question-0']:checked"), function() {
+  			if ($(this).val() == questions[0].correctAnswer) {
+  				console.log(this);
+  				  game.correct++;
+  			} else {
+  				game.incorrect++;
+  			}
+
+  		});
+  		$.each($("input[name='question-1']:checked"), function() {
+  			if ($(this).val() == questions[1].correctAnswer) {
+  				console.log(this);
+  				game.correct++;
+  			} else {
+  				game.incorrect++;
+  			}
+
+  		});
+  		$.each($("input[name='question-2']:checked"), function() {
+  			if ($(this).val() == questions[2].correctAnswer) {
+  				console.log(this);
+  				game.correct++;
+  			} else {
+  				game.incorrect++;
+  			}
+
+  		});
+  		$.each($("input[name='question-3']:checked"), function() {
+  			if ($(this).val() == questions[3].correctAnswer) {
+  				console.log(this);
+  				game.correct++;
+  			} else {
+  				game.incorrect++;
+  			}
+
+  		});
+  		$.each($("input[name='question-4']:checked"), function() {
+  			if ($(this).val() == questions[4].correctAnswer) {
+  				console.log(this);
+  				game.correct++;
+  			} else {
+  				game.incorrect++;
+  			}
+
+  		});
+
+  		this.results();
+  	},
+
+
+  	  results:function() {
+  	  	clearInterval(timer);
+
+  	  	$("#subcontainer h2").remove();
+  	   panel.html("<h2>You're Done!</h2>");
+  	   panel.append("<h3>You got this many right: " + this.correct + "</h3>");
+  	   panel.append("<h3>You got this many wrong: " + this.incorrect + "</h3>");
+  	   panel.append("<h3>You didn't answer this many: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  	  
+  	  }
+
+  }
